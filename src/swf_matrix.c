8a820c456cc3f50ea9351addb2d2c8ab6d80d346
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2009-06-03 16:37:40 +0000

    swf_matrix.c の追加 (空に関数だけ用意)
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@140 7c90b180-03d5-4157-b861-58a559ae9d1e

---
+++ src/swf_matrix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "bitstream.h"
+#include "swf_matrix.h"
+
+int
+swf_matrix_parse(bitstream_t *bs, swf_matrix_t *matrix) {
+    return 0;
+}
+
+int
+swf_matrix_build(bitstream_t *bs, swf_matrix_t *matrix) {
+    return 0;
+}
+
+int
+swf_matrix_print(swf_matrix_t *matrix, int indent_depth) {
+    print_indent(indent_depth);
+    return 0;
+}
