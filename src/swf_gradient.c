91b78930bd282d65b3915c30961d3af43e68fc56
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2009-06-03 16:35:19 +0000

    swf_gradient.c の追加 (空の関数だけ用意)
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@139 7c90b180-03d5-4157-b861-58a559ae9d1e

---
+++ src/swf_gradient.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "bitstream.h"
+#include "swf_gradient.h"
+
+int
+swf_gradient_parse(bitstream_t *bs, swf_gradient_t *gradient) {
+    return 0;
+}
+
+int
+swf_gradient_build(bitstream_t *bs, swf_gradient_t *gradient) {
+    return 0;
+}
+
+int
+swf_gradient_print(swf_gradient_t *gradient, int indent_depth) {
+    print_indent(indent_depth);
+    return 0;
+}
