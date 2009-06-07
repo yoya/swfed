8d603607d46c13c204ea44fa67e251a666ab943a
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2009-06-07 17:29:54 +0000

    swf_shape_record_edge_parse を実装
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@153 7c90b180-03d5-4157-b861-58a559ae9d1e

--- src/swf_shape_record_edge.c
+++ src/swf_shape_record_edge.c
@@ -2,20 +2,46 @@
 #include "bitstream.h"
 #include "swf_shape_record_edge.h"
 
+int
 swf_shape_record_edge_parse(bitstream_t *bs,
                             swf_shape_record_edge_t *shape_record_edge) {
-    
+    unsigned int shape_coord_real_size;
+    shape_record_edge->shape_record_type = bitstream_getbit(bs);
+    shape_record_edge->shape_edge_type   = bitstream_getbit(bs);
+    shape_record_edge->shape_coord_size  = bitstream_getbits(bs, 4);
+    shape_coord_real_size = shape_record_edge->shape_coord_size + 2;
+    if (shape_record_edge->shape_edge_type == 0) {
+        shape_record_edge->shape_control_delta_x = bitstream_getbits(bs, shape_coord_real_size);
+        shape_record_edge->shape_control_delta_y = bitstream_getbits(bs, shape_coord_real_size);
+        shape_record_edge->shape_anchor_delta_x = bitstream_getbits(bs, shape_coord_real_size);
+        shape_record_edge->shape_anchor_delta_y = bitstream_getbits(bs, shape_coord_real_size);
+    } else {
+        shape_record_edge->shape_line_has_x_and_y = bitstream_getbit(bs);
+        if (shape_record_edge->shape_line_has_x_and_y == 1) {
+            shape_record_edge->shape_delta_x = bitstream_getbits(bs, shape_coord_real_size);
+            shape_record_edge->shape_delta_y = bitstream_getbits(bs, shape_coord_real_size);
+        } else {
+            shape_record_edge->shape_line_has_x_or_y = bitstream_getbit(bs);
+            if (shape_record_edge->shape_line_has_x_or_y == 0) {
+                shape_record_edge->shape_delta_x = bitstream_getbits(bs, shape_coord_real_size);
+            } else {
+                shape_record_edge->shape_delta_y = bitstream_getbits(bs, shape_coord_real_size);
+            }
+        }
+    }
     return 0;
 }
 
 int
 swf_shape_record_edge_build(bitstream_t *bs,
                             swf_shape_record_edge_t *shape_record_edge) {
+    fprintf(stderr, "swf_shape_record_edge_build: not implemented yet\n");
     return 0;
 }
 
 int
 swf_shape_record_edge_print(swf_shape_record_edge_t *shape_record_edge,
                             int indent_depth) {
+    fprintf(stderr, "swf_shape_record_edge_print: not implemented yet\n");
     return 0;
 }
