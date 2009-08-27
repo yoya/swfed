690bdea0b8031de5c6cf6b48fa5f521ba8bf5292
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2009-08-27 11:36:44 +0000

    _print での RGBA 表示を簡略化
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@173 7c90b180-03d5-4157-b861-58a559ae9d1e

--- src/swf_rgb.c
+++ src/swf_rgb.c
@@ -21,7 +21,7 @@ swf_rgb_build(bitstream_t *bs, swf_rgb_t *color) {
 int
 swf_rgb_print(swf_rgb_t *color, int indent_depth) {
     print_indent(indent_depth);
-    printf("red=0x%02x  green=0x%02X  blue=0x%02x\n",
+    printf("RGB=#%02x%02x%02x\n",
            color->red, color->green, color->blue);
     return 0;
 }
