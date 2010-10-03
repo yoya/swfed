a2443aff1bd4df2d179a0d5199335cef5ffb2032
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2010-10-03 14:29:18 +0000

    変数名のtypo修正。インデントの改良
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@206 7c90b180-03d5-4157-b861-58a559ae9d1e

--- src/swf_line_style_array.c
+++ src/swf_line_style_array.c
@@ -5,7 +5,7 @@
 
 int
 swf_line_style_array_parse(bitstream_t *bs,
-                           swf_line_style_array_t *shape_with_style,
+                           swf_line_style_array_t *line_style_array,
                            swf_tag_t *tag) {
     int i;
     int result;
@@ -14,29 +14,29 @@ swf_line_style_array_parse(bitstream_t *bs,
         fprintf(stderr, "swf_line_style_array_parse: bitstream_getbyte failed at (L%d)\n", __LINE__);
         return 1;
     }
-    shape_with_style->count = result;
+    line_style_array->count = result;
     if ((tag->tag != 2) && // ! DefineShape
-         (shape_with_style->count == 255)) {
+         (line_style_array->count == 255)) {
         result = bitstream_getbytesLE(bs, 2);
         if (result == -1) {
             fprintf(stderr, "swf_line_style_array_parse: bitstream_getbyte failed at (L%d)\n", __LINE__);
             return 1;
         }
-        shape_with_style->count = result;
+        line_style_array->count = result;
     }
-    if (1000 < shape_with_style->count) { // XXX
+    if (10000 < line_style_array->count) { // XXX
         fprintf(stderr, "swf_line_style_array_parse: too many count(%d)\n",
-                shape_with_style->count);
-        shape_with_style->count = 0;
+                line_style_array->count);
+        line_style_array->count = 0;
         return 1;
     }
-    shape_with_style->line_style = calloc(shape_with_style->count, sizeof(swf_line_style_t));
-    for (i = 0 ; i < shape_with_style->count ; i++) {
-        result = swf_line_style_parse(bs, &(shape_with_style->line_style[i]), tag);
+    line_style_array->line_style = calloc(line_style_array->count, sizeof(swf_line_style_t));
+    for (i = 0 ; i < line_style_array->count ; i++) {
+        result = swf_line_style_parse(bs, &(line_style_array->line_style[i]), tag);
         if (result) {
-            fprintf(stderr, "swf_line_style_array_parse: swf_line_style_parse failed shape_with_style->count %d => %d\n",
-                    shape_with_style->count, i);
-            shape_with_style->count = i; // XXX
+            fprintf(stderr, "swf_line_style_array_parse: swf_line_style_parse failed, change line_style->count %d => %d\n",
+                    line_style_array->count, i);
+            line_style_array->count = i; // XXX
             return result;
         }
     }
@@ -44,29 +44,29 @@ swf_line_style_array_parse(bitstream_t *bs,
 }
 
 int
-swf_line_style_array_build(bitstream_t *bs, swf_line_style_array_t *shape_with_style, swf_tag_t *tag) {
+swf_line_style_array_build(bitstream_t *bs, swf_line_style_array_t *line_style_array, swf_tag_t *tag) {
     int i;
     if ((tag->tag != 2) || // ! DefineShape
-        (255 <= shape_with_style->count)) {
+        (255 <= line_style_array->count)) {
         bitstream_putbyte(bs, 255);
-        bitstream_putbytesLE(bs, shape_with_style->count, 2);
+        bitstream_putbytesLE(bs, line_style_array->count, 2);
     } else {
-        bitstream_putbyte(bs, shape_with_style->count);
+        bitstream_putbyte(bs, line_style_array->count);
     }
-    for (i = 0 ; i < shape_with_style->count ; i++) {
-        swf_line_style_build(bs, &(shape_with_style->line_style[i]), tag);
+    for (i = 0 ; i < line_style_array->count ; i++) {
+        swf_line_style_build(bs, &(line_style_array->line_style[i]), tag);
     }
     return 0;
 }
 
 int
-swf_line_style_array_print(swf_line_style_array_t *shape_with_style, int indent_depth, swf_tag_t *tag) {
+swf_line_style_array_print(swf_line_style_array_t *line_style_array, int indent_depth, swf_tag_t *tag) {
     int i;
-    print_indent(indent_depth);
-    printf("shape_with_style->count=%u\n", shape_with_style->count);
-    for (i = 0 ; i < shape_with_style->count ; i++) {
-        swf_line_style_print(&(shape_with_style->line_style[i]),
-                             indent_depth + 1, tag);
+    print_indent(indent_depth + 1);
+    printf("line_style_array->count=%u\n", line_style_array->count);
+    for (i = 0 ; i < line_style_array->count ; i++) {
+        swf_line_style_print(&(line_style_array->line_style[i]),
+                             indent_depth + 2, tag);
     }
     return 0;
 }
