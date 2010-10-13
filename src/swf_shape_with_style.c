4ce28a720aa6aaf8308a89dfdc4fb15844cd4dfb
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2010-10-13 16:43:48 +0000

    エラー出力追加
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@249 7c90b180-03d5-4157-b861-58a559ae9d1e

--- src/swf_shape_with_style.c
+++ src/swf_shape_with_style.c
@@ -11,11 +11,16 @@ swf_shape_with_style_parse(bitstream_t *bs,
     swf_tag_shape_detail_t *swf_tag_shape = (swf_tag_shape_detail_t *) tag->detail;
     result = swf_styles_parse(bs, &(shape_with_style->styles), tag);
     if (result) {
+        fprintf(stderr, "swf_shape_with_style_parse: swf_styles_parse failed\n");
         return result;
     }
     swf_tag_shape->_current_styles_count = shape_with_style->styles.styles_count; // XXX
     result = swf_shape_record_parse(bs, &(shape_with_style->shape_records),
                                     tag);
+    if (result) {
+        fprintf(stderr, "swf_shape_with_style_parse: swf_shape_record_parse failed\n");
+        return result;
+    }
     return result;
 }
 
@@ -24,10 +29,12 @@ swf_shape_with_style_build(bitstream_t *bs, swf_shape_with_style_t *shape_with_s
     int ret;
     ret = swf_styles_build(bs, &(shape_with_style->styles), tag);
     if (ret) {
+        fprintf(stderr, "swf_shape_with_style_build: swf_styles_build failed\n");
         return ret;
     }
     ret = swf_shape_record_build(bs, &(shape_with_style->shape_records), tag);
     if (ret) {
+        fprintf(stderr, "swf_shape_with_style_build: swf_shape_record_build failed\n");
         return ret;
     }
     return 0;
