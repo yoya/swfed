3ef074c1bd6c15a2af05bd484872d2a83f01e106
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2010-10-13 16:37:09 +0000

    Shape Matrix のスケール/回転/位相変換ツール
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@248 7c90b180-03d5-4157-b861-58a559ae9d1e

---
+++ sample/swfapplyshapematrix.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($argc != 8) {
+    fprintf(STDERR, "Usage: swfapplyshapematrix <swf_file> <shape_id> <scale_x> <scale_y> <radian> <trans_x> <trans_y>\n");
+    exit(1);
+}
+list($progname, $swf_filename, $shape_id, $scale_x, $scale_y, $radian, $trans_x, $trans_y) = $argv;
+
+$swfdata = file_get_contents($swf_filename);
+
+$obj = new SWFEditor();
+if ($obj->input($swfdata) == false) {
+    fprintf(STDERR, "input failed\n");
+    exit(1);
+}
+
+$result = $obj->applyShapeMatrixFactor($shape_id, $scale_x, $scale_y, $radian, $trans_x, $trans_y);
+
+if ($result == false) {
+    fprintf(STDERR, "applyShapeMatrixFactor($shape_id, ...) failed\n");
+    exit(1);
+}
+
+echo $obj->output();
