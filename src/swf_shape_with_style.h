988151600e3f9b9c67cccc4f6071b4aa9449fdc5
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2010-10-25 16:31:01 +0000

    swf_shape_with_style_delete を実装
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@288 7c90b180-03d5-4157-b861-58a559ae9d1e

--- src/swf_shape_with_style.h
+++ src/swf_shape_with_style.h
@@ -17,13 +17,14 @@ typedef struct swf_shape_with_style_ {
 } swf_shape_with_style_t;
 
 extern int swf_shape_with_style_parse(bitstream_t *bs,
-                                      swf_shape_with_style_t *color,
+                                      swf_shape_with_style_t *shape_with_style,
                                       swf_tag_t *tag);
 extern int swf_shape_with_style_build(bitstream_t *bs,
-                                      swf_shape_with_style_t *color,
+                                      swf_shape_with_style_t *shape_with_style,
                                       swf_tag_t *tag);
-extern int swf_shape_with_style_print(swf_shape_with_style_t *color,
+extern int swf_shape_with_style_print(swf_shape_with_style_t *shape_with_style,
                                       int indent_depth,
                                       swf_tag_t *tag);
+extern int swf_shape_with_style_delete(swf_shape_with_style_t *shape_with_style);
 
 #endif /* __SWF_SHAPE_WITH_STYLE_H__ */
