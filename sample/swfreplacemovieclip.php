a83417d0457676bf390964e1f98d482e60665b69
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2011-03-07 17:20:14 +0000

    replaceMovieClip のテストスクリプト
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@394 7c90b180-03d5-4157-b861-58a559ae9d1e

---
+++ sample/swfreplacemovieclip.php
@@ -0,0 +1,30 @@
+<?php
+
+if (($argc < 4) || ($argc%2 != 0)) {
+	fprintf(STDERR, "Usage: swfreplacemovieclip <swf_file> <instance_name> <movieclip_swffile> [<instance_name2> <movieclip_swffile2> [...]]\n");
+	exit(1);
+}
+
+$swf_filename = $argv[1];
+
+$swfdata = file_get_contents($swf_filename);
+$obj = new SWFEditor();
+
+if ($obj->input($swfdata) == false) {
+	fprintf(STDERR, "input failed\n");
+	exit(1);
+}
+
+for ($i=2 ; $i < $argc ; $i+=2) {
+	$instance_name = $argv[$i];
+	$movieclip_swffilename = $argv[$i+1];
+	$movieclip_swfdata = file_get_contents($movieclip_swffilename);
+	if ($obj->replaceMovieClip($instance_name, $movieclip_swfdata) == false) {
+		fprintf(STDERR, "replaceMovieClip($instance_name, ...) failed\n");
+		exit(1);
+	}
+}
+
+echo $obj->output();
+
+$obj->_destroy_and_exit();
