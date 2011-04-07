d898c9bcd97e49269bfb376c0bb7d5e016a9acdd
yoya <yoya@7c90b180-03d5-4157-b861-58a559ae9d1e>
2011-04-07 03:58:20 +0000

    replaceShapeData 失敗時にエラー出力 (apache error.log に出るのを期待して)
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/swfed/trunk@531 7c90b180-03d5-4157-b861-58a559ae9d1e

--- www/swfshape.php
+++ www/swfshape.php
@@ -13,7 +13,9 @@ $swf_data = file_get_contents("shapetpl.swf");
 
 $swf = new SWFEditor();
 $swf->input($swf_data);
-$swf->replaceShapeData(1, $shape_data);
+if ($swf->replaceShapeData(1, $shape_data) === false) {
+    fprintf(STDERR, "replaceShapeData(1, ...) failed. id=$id\n");
+}
 
 header("Content-type: application/x-shockwave-flash");
 $header_info = array(
