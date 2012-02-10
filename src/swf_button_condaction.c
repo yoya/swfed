09ecb42757bfea679369b54c7d69a3b1dbb5cdad
Yoshihiro Yamazaki <yoya@awm.jp>
2012-02-10 20:32:06 +0900

    delete list function

--- src/swf_button_condaction.c
+++ src/swf_button_condaction.c
@@ -25,32 +25,3 @@ void
 swf_button_condaction_destroy(swf_button_condaction_t *button_condaction) {
     ;
 }
-
-/*
- * swf_button_condaction_list
- */
-
-swf_button_condaction_list_t *
-swf_button_condaction_list_create(void) {
-    ;
-}
-
-int
-swf_button_condaction_list_parse(bitstream_t *bs, swf_button_condaction_list_t *button_condaction_list) {
-    ;
-}
-
-int
-swf_button_condaction_list_build(bitstream_t *bs, swf_button_condaction_list_t *button_condaction_list) {
-    ;
-}
-
-int
-swf_button_condaction_list_print(swf_button_condaction_list_t *button_condaction_list, int indent_depth) {
-    ;
-}
-
-void
-swf_button_condaction_list_destroy(swf_button_condaction_list_t *button_condaction_list) {
-    ;
-}
--- src/swf_button_condaction.h
+++ src/swf_button_condaction.h
@@ -36,10 +36,4 @@ extern int swf_button_condaction_build(bitstream_t *bs, swf_button_condaction_t 
 extern int swf_button_condaction_print(swf_button_condaction_t *button_condaction, int indent_depth);
 extern void swf_button_condaction_destroy(swf_button_condaction_t *button_condaction);
 
-extern swf_button_condaction_list_t *swf_button_condaction_list_create(void);
-extern int swf_button_condaction_list_parse(bitstream_t *bs, swf_button_condaction_list_t *button_condaction_list);
-extern int swf_button_condaction_list_build(bitstream_t *bs, swf_button_condaction_list_t *button_condaction_list);
-extern int swf_button_condaction_list_print(swf_button_condaction_list_t *button_condaction_list, int indent_depth);
-extern void swf_button_condaction_list_destroy(swf_button_condaction_list_t *button_condaction_list);
-
 #endif /* __SWF_BUTTON_CONDACTION_H__ */
