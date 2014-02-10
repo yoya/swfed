08fcbde42ad1b0e54897ff8187cc9059b9fb0930
yoya <yoya@awm.jp>
2014-02-10 11:32:16 +0900

    update SWFED_VERSION from 0.62a to 0.62

--- src/php_swfed.c
+++ src/php_swfed.c
@@ -42,7 +42,7 @@
 #include "swf_tag.h"
 #include "swf_object.h"
 
-#define SWFED_VERSION "0.62a"
+#define SWFED_VERSION "0.62"
 
 #define get_zend_hash_value_long(table, key, value) do { \
         zval **tmp = NULL; \
